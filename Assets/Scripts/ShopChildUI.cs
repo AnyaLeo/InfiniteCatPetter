f402a88ef76b6de015f1a700a8259d5efe341cdf
JuSung Lee <jusunglee1459@gmail.com>
2021-09-18 21:17:54 -0700

    Shop UI tabs

---
+++ Assets/Scripts/ShopChildUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopChildUI : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        transform.parent.GetComponent<ShopUI>().childClicked(transform.gameObject);
+    }
+}
