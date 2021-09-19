13f85b6c01defb85888745b868547c3dd9810542
JuSung Lee <jusunglee1459@gmail.com>
2021-09-18 22:12:24 -0700

    cursor changed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,10 @@ public class GameManager : Singleton<GameManager>
     public int currentLevel;
     public int maxLevels = 5;
 
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
     public void IncrementCurrentLevel()
     {
         currentLevel++;
@@ -19,5 +23,17 @@ public class GameManager : Singleton<GameManager>
         {
             // trigger the apocalypse
         }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            changeCursor(cursorTexture);
+        }
+    }
+
+    public void changeCursor(Texture2D texture)
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
     }
 }
