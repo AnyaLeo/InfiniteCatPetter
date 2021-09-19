03d57dcd465b6a8bc1b07efe5fabfa8e590bb4de
JuSung Lee <jusunglee1459@gmail.com>
2021-09-19 03:10:50 -0700

    item popup ui cleanup

--- Assets/Scripts/ShopItemUI.cs
+++ Assets/Scripts/ShopItemUI.cs
@@ -15,12 +15,19 @@ public class ShopItemUI : MonoBehaviour
     private string itemTitle;
     [SerializeField]
     private string itemDescription;
+
     private void OnMouseEnter()
     {
+        popUpDescription.SetActive(true);
         itemTitleUI.GetComponentInParent<Text>().text = itemTitle;
         itemdescriptorUI.GetComponentInParent<Text>().text = itemDescription;
         Vector3 cursorVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 popUpvector = new Vector3(cursorVector.x, cursorVector.y, 0);
         popUpDescription.transform.SetPositionAndRotation(popUpvector, popUpDescription.transform.rotation);
+    }
+
+    private void OnMouseExit()
+    {
+        popUpDescription.SetActive(false);
     }
 }
