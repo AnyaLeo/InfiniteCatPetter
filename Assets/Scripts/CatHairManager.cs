38da5e5543857aa127eb18a378b70410fec12019
Anya <ania.leonova@gmail.com>
2021-09-19 09:51:32 -0700

    Added a cat hair indicator on the screen

--- Assets/Scripts/CatHairManager.cs
+++ Assets/Scripts/CatHairManager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CatHairManager : Singleton<CatHairManager> 
 {
     public float currentCatHair;
-    
+
+    public TextMeshProUGUI catHairText;
+
+    private void Start()
+    {
+        catHairText.text = currentCatHair.ToString("f0");
+    }
 
     public void ChangeCatHair(float difference)
     {
         currentCatHair += difference;
+
+        catHairText.text = currentCatHair.ToString("f0");
     }
 
    
