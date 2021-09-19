0ae9cee05ab6e5a6b1f40d38f4311106e4e9c3e3
JuSung Lee <jusunglee1459@gmail.com>
2021-09-19 10:50:08 -0700

    popup offset

--- Assets/Scripts/ShopItemUI.cs
+++ Assets/Scripts/ShopItemUI.cs
@@ -17,13 +17,22 @@ public class ShopItemUI : MonoBehaviour
     [SerializeField]
     private string itemDescription;
 
+    private float popupHeight;
+    private float popupWidth;
+
+    public void Awake()
+    {
+        popupHeight = popUpDescription.transform.GetComponent<RectTransform>().sizeDelta.x;
+        popupWidth = popUpDescription.transform.GetComponent<RectTransform>().sizeDelta.y;
+    }
+
     private void OnMouseEnter()
     {
         popUpDescription.SetActive(true);
         itemTitleUI.GetComponentInParent<TextMeshProUGUI>().text = itemTitle;
         itemdescriptorUI.GetComponentInParent<TextMeshProUGUI>().text = itemDescription;
         Vector3 cursorVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 popUpvector = new Vector3(cursorVector.x, cursorVector.y, 0);
+        Vector3 popUpvector = new Vector3(cursorVector.x + 1, cursorVector.y + 1, 0);
         popUpDescription.transform.SetPositionAndRotation(popUpvector, popUpDescription.transform.rotation);
     }
 
