c5f8958a85ea17d5ec1f4891bf01d50644549544
JuSung Lee <jusunglee1459@gmail.com>
2021-09-19 03:03:10 -0700

    shop popup

--- Assets/Scripts/ShopChildUI.cs
+++ Assets/Scripts/ShopChildUI.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopChildUI : MonoBehaviour
-{
+{   
+    [SerializeField]
+    private GameObject popUpDescription;
+
     private void OnMouseDown()
     {
         transform.parent.GetComponent<ShopUI>().childClicked(transform.gameObject);
---
+++ Assets/Scripts/ShopItemUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItemUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject popUpDescription;
+    [SerializeField]
+    private GameObject itemTitleUI;
+    [SerializeField]
+    private GameObject itemdescriptorUI;
+    [SerializeField]
+    private string itemTitle;
+    [SerializeField]
+    private string itemDescription;
+    private void OnMouseEnter()
+    {
+        itemTitleUI.GetComponentInParent<Text>().text = itemTitle;
+        itemdescriptorUI.GetComponentInParent<Text>().text = itemDescription;
+        Vector3 cursorVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 popUpvector = new Vector3(cursorVector.x, cursorVector.y, 0);
+        popUpDescription.transform.SetPositionAndRotation(popUpvector, popUpDescription.transform.rotation);
+    }
+}
